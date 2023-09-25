694d37c6ccd79e9b84d2ef170fb9dfffabfd2780
TrustyTristan <tristanbrazier@gmail.com>
2023-09-25 20:09:21 +0000

    Fixed response when no details from jamf

--- src/JamfPSPro/Private/Get-ErrorMessage.ps1
+++ src/JamfPSPro/Private/Get-ErrorMessage.ps1
@@ -10,20 +10,24 @@ function Get-ErrorMessage {
     )
     PROCESS {
         # Extract the message from $ErrorDetails.ErrorDetails.Message
-        $startMarker = $ErrorDetails.Exception.Response.StatusCode.ToString() -replace '^.*(?=.{3}$)' 
-        $endMarker = "You can get technical details here"
+        $StartMarker = $ErrorDetails.Exception.Response.StatusCode.ToString() -replace '^.*(?=.{3}$)' 
+        $EndMarker = "You can get technical details here"
         # Find the start and end positions of the markers
-        $startPos = $ErrorDetails.ErrorDetails.Message.IndexOf($startMarker)
-        $endPos = $ErrorDetails.ErrorDetails.Message.IndexOf($endMarker, $startPos)
-        if ($startPos -ge 0 -and $endPos -ge 0) {
-            $message = $ErrorDetails.ErrorDetails.Message.Substring($startPos + $startMarker.Length, $endPos - $startPos - $startMarker.Length).Trim()
+        if ( $ErrorDetails.ErrorDetails.Message.Contains($EndMarker) ) {
+            $StartPos = $ErrorDetails.ErrorDetails.Message.IndexOf($StartMarker)
+            $EndPos = $ErrorDetails.ErrorDetails.Message.IndexOf($EndMarker, $StartPos)
+            if ( $StartPos -ge 0 -and $EndPos -ge 0 ) {
+                $Message = $ErrorDetails.ErrorDetails.Message.Substring($StartPos + $StartMarker.Length, $EndPos - $StartPos - $StartMarker.Length).Trim()
+            }
+        } else {
+            $Message = "Unable to determine error message"
         }
 
         $ErrorMessage = [PSCustomObject]@{
             requestUri  = $ErrorDetails.Exception.Response.RequestMessage.RequestUri.AbsoluteUri
             httpStatus  = $ErrorDetails.Exception.Response.StatusCode.Value__
             description = $ErrorDetails.Exception.Response.StatusCode
-            message     = $message
+            message     = $Message
         }
 
         try {
