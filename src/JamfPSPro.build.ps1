c9932ae1bc7894897c7e263c96b26a7439be9b4e
TrustyTristan <tristanbrazier@gmail.com>
2024-10-04 04:58:35 +0000

    Fix for new common action 'ProgressAction'

--- src/JamfPSPro.build.ps1
+++ src/JamfPSPro.build.ps1
@@ -349,6 +349,132 @@ Add-BuildTask CreateMarkdownHelp -After CreateHelpStart {
     $null = New-MarkdownHelp @markdownParams
     Write-Build Gray '           ...Markdown generation completed.'
 
+    ### PROGRESS ACTION FIX ###
+    # modified from source: https://github.com/PowerShell/platyPS/issues/595#issuecomment-1820971702
+    function Remove-CommonParameterFromMarkdown {
+        <#
+            .SYNOPSIS
+                Remove a PlatyPS generated parameter block.
+
+            .DESCRIPTION
+                Removes parameter block for the provided parameter name from the markdown file provided.
+
+        #>
+        param(
+            [Parameter(Mandatory)]
+            [string[]]
+            $Path,
+
+            [Parameter(Mandatory = $false)]
+            [string[]]
+            $ParameterName = @('ProgressAction')
+        )
+        $ErrorActionPreference = 'Stop'
+        foreach ($p in $Path) {
+            $content = (Get-Content -Path $p -Raw).TrimEnd()
+            $updateFile = $false
+            foreach ($param in $ParameterName) {
+                if (-not ($Param.StartsWith('-'))) {
+                    $param = "-$($param)"
+                }
+                # Remove the parameter block
+                $pattern = "(?m)^### $param\r?\n[\S\s]*?(?=#{2,3}?)"
+                $newContent = $content -replace $pattern, ''
+                # Remove the parameter from the syntax block
+                $pattern = " \[$param\s?.*?]"
+                $newContent = $newContent -replace $pattern, ''
+                if ($null -ne (Compare-Object -ReferenceObject $content -DifferenceObject $newContent)) {
+                    Write-Verbose "Added $param to $p"
+                    # Update file content
+                    $content = $newContent
+                    $updateFile = $true
+                }
+            }
+            # Save file if content has changed
+            if ($updateFile) {
+                $newContent | Out-File -Encoding utf8 -FilePath $p
+                Write-Verbose "Updated file: $p"
+            }
+        }
+        return
+    }
+
+    function Add-MissingCommonParameterToMarkdown {
+        param(
+            [Parameter(Mandatory)]
+            [string[]]
+            $Path,
+
+            [Parameter(Mandatory = $false)]
+            [string[]]
+            $ParameterName = @('ProgressAction')
+        )
+        $ErrorActionPreference = 'Stop'
+        foreach ($p in $Path) {
+            $content = (Get-Content -Path $p -Raw).TrimEnd()
+            $updateFile = $false
+            foreach ($NewParameter in $ParameterName) {
+                if (-not ($NewParameter.StartsWith('-'))) {
+                    $NewParameter = "-$($NewParameter)"
+                }
+                $pattern = '(?m)^This cmdlet supports the common parameters:(.+?)\.'
+                $replacement = {
+                    $Params = $_.Groups[1].Captures[0].ToString() -split ' '
+                    $CommonParameters = @()
+                    foreach ($CommonParameter in $Params) {
+                        if ($CommonParameter.StartsWith('-')) {
+                            if ($CommonParameter.EndsWith(',')) {
+                                $CleanParam = $CommonParameter.Substring(0, $CommonParameter.Length -1)
+                            } elseif ($p.EndsWith('.')) {
+                                $CleanParam = $CommonParameter.Substring(0, $CommonParameter.Length -1)
+                            } else{
+                                $CleanParam = $CommonParameter
+                            }
+                            $CommonParameters += $CleanParam
+                        }
+                    }
+                    if ($NewParameter -notin $CommonParameters) {
+                        $CommonParameters += $NewParameter
+                    }
+                    $CommonParameters[-1] = "and $($CommonParameters[-1]). "
+                    return "This cmdlet supports the common parameters: " + (($CommonParameters | Sort-Object) -join ', ')
+                }
+                $newContent = $content -replace $pattern, $replacement
+                if ($null -ne (Compare-Object -ReferenceObject $content -DifferenceObject $newContent)) {
+                    Write-Verbose "Added $NewParameter to $p"
+                    $updateFile = $true
+                    $content = $newContent
+                }
+            }
+            # Save file if content has changed
+            if ($updateFile) {
+                $newContent | Out-File -Encoding utf8 -FilePath $p
+                Write-Verbose "Updated file: $p"
+            }
+        }
+        return
+    }
+
+    function Repair-PlatyPSMarkdown {
+        param(
+            [Parameter(Mandatory)]
+            [string[]]
+            $Path,
+
+            [Parameter()]
+            [string[]]
+            $ParameterName = @('ProgressAction')
+        )
+        $ErrorActionPreference = 'Stop'
+        $Parameters = @{
+            Path = $Path
+            ParameterName = $ParameterName
+        }
+        $null = Remove-CommonParameterFromMarkdown @Parameters
+        $null = Add-MissingCommonParameterToMarkdown @Parameters
+        return
+    }
+    ### PROGRESS ACTION FIX ###
     Write-Build Gray '           Replacing markdown elements...'
     # Replace multi-line EXAMPLES
     $OutputDir = "$script:ArtifactsPath\docs\"
@@ -359,6 +485,7 @@ Add-BuildTask CreateMarkdownHelp -After CreateHelpStart {
         if ($newContent -ne $content) {
             Set-Content -Path $_.FullName -Value $newContent -Force
         }
+        Repair-PlatyPSMarkdown $_.FullName
     }
     # Replace each missing element we need for a proper generic module page .md file
     $ModulePageFileContent = Get-Content -Raw $ModulePage
