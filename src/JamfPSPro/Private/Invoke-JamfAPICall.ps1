40fcb6c169d888e320e9a1026d449671cb67fbc7
TrustyTristan <tristanbrazier@gmail.com>
2024-10-04 03:11:00 +0000

    Made changed to hopefully handle paginated responses

--- src/JamfPSPro/Private/Invoke-JamfAPICall.ps1
+++ src/JamfPSPro/Private/Invoke-JamfAPICall.ps1
@@ -41,33 +41,54 @@ function Invoke-JamfAPICall {
                 $Response = Invoke-RestMethod $Path -Authentication Bearer -Token $TokenJamfPSPro.token -ContentType $app_Type -Headers $app_Headers -Method $Method -Body $Body -ErrorAction Stop
             } else {
                 $Response = Invoke-RestMethod $Path -Authentication Bearer -Token $TokenJamfPSPro.token -ContentType $app_Type -Headers $app_Headers -Method $Method -ErrorAction Stop
-            }
-
-            # totalCount is unnecessary
-            $Response.PSObject.Properties.Remove('totalCount')
-
-            # Expand result if only 1 property at top level
-            if ( ($Response.PSObject.Properties | Measure-Object).Count -eq 1 ) {
-                $Response = ($Response | Where-Object {$_.getType().Name -eq 'PSCustomObject'}).PSObject.Properties.Value
-                # If response is empty, but still valid
-                if ( -not $Response ) {
-                    $Response = [PSCustomObject]@{}
+                $pageCount = ($Response.results.count)
+                if ( $Response.totalCount -gt $pageCount ) {
+                    $ResponseList = New-Object System.Collections.Generic.List[System.Object]
+                    $Pages = [math]::ceiling( $Response.totalCount / $pageCount ) - 1
+                    $Response | Add-Member -NotePropertyName 'pageCount' -NotePropertyValue $pageCount
+                    $ResponseList.Add( $Response )
+                    foreach ( $Page in $Pages ) {
+                        $Response = Invoke-RestMethod "$Path/?page=$Page" -Authentication Bearer -Token $TokenJamfPSPro.token -ContentType $app_Type -Headers $app_Headers -Method $Method -ErrorAction Stop
+                        $Response | Add-Member -NotePropertyName 'pageCount' -NotePropertyValue ($Response.results.count)
+                        $ResponseList.Add( $Response )
+                    }
                 }
             }
 
-            # No response with delete, don't know of a way to validate success
-            if ( $Method -eq 'delete' ) {
-                $Response = [PSCustomObject]@{
-                    IsSuccessStatusCode = $true
+            if ( $ResponseList ) {
+                $ResponseList = $ResponseList | Select-Object * -ExcludeProperty totalCount, pageCount
+                $ResponseName = $ResponseList | Get-Member -MemberType NoteProperty
+                if ( $ResponseName.Count -eq 1) {
+                    $ResponseList.($ResponseName.Name) | Add-Member -NotePropertyName 'IsSuccessStatusCode' -NotePropertyValue $true
+                    return $ResponseList.($ResponseName.Name)
+                } else {
+                    $ResponseList | Add-Member -NotePropertyName 'IsSuccessStatusCode' -NotePropertyValue $true
+                    return $ResponseList
                 }
             } else {
-                $Response | Add-Member -NotePropertyName 'IsSuccessStatusCode' -NotePropertyValue $true
+                # totalCount is unnecessary
+                $Response.PSObject.Properties.Remove('totalCount')
+                # Expand result if only 1 property at top level
+                if ( ($Response.PSObject.Properties | Measure-Object).Count -eq 1 ) {
+                    $Response = ($Response | Where-Object {$_.getType().Name -eq 'PSCustomObject'}).PSObject.Properties.Value
+                    # If response is empty, but still valid
+                    if ( -not $Response ) {
+                        $Response = [PSCustomObject]@{}
+                    }
+                }
+                # No response with delete, don't know of a way to validate success
+                if ( $Method -eq 'delete' ) {
+                    $Response = [PSCustomObject]@{
+                        IsSuccessStatusCode = $true
+                    }
+                } else {
+                    $Response | Add-Member -NotePropertyName 'IsSuccessStatusCode' -NotePropertyValue $true
+                }
+                return $Response
             }
-
-            return $Response
         } catch {
             $ErrorMessage = $_
-            Add-Member -InputObject $ErrorMessage -NotePropertyName IsSuccessStatusCode -NotePropertyValue $false
+            Add-Member -InputObject $ErrorMessage -NotePropertyName 'IsSuccessStatusCode' -NotePropertyValue $false
             return $ErrorMessage
         }
     }
